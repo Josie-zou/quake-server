ed97f16dcc5ff9606415d0de6f53fcb43ffef126
Josie <zou_josie@163.com>
2016-05-21 17:47:44 +0800

    update .gitignore

---
+++ src/main/java/com/josie/quake/dao/FilterWhiteListDao.java
@@ -0,0 +1,10 @@
+package com.josie.quake.dao;
+
+import com.josie.quake.dao.annotation.DataSourceQuake;
+
+/**
+ * Created by Josie on 16/5/21.
+ */
+@DataSourceQuake
+public interface FilterWhiteListDao {
+}
---
+++ src/main/java/com/josie/quake/model/FilterWhiteList.java
@@ -0,0 +1,81 @@
+package com.josie.quake.model;
+
+import java.util.Date;
+
+/**
+ * Created by Josie on 16/5/21.
+ */
+public class FilterWhiteList {
+    private int id;
+    private String url;
+    private int operator;
+    private int status;
+    private Date createTime;
+
+    public static enum Status {
+
+        Disable(2), Enable(1);
+
+        private int value;
+
+        Status(
+                int value) {
+            this.value = value;
+        }
+
+        public int toInt() {
+            return value;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getOperator() {
+        return operator;
+    }
+
+    public void setOperator(int operator) {
+        this.operator = operator;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "FilterWhiteList{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", operator=" + operator +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
