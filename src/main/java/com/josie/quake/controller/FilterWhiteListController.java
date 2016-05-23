8961072fbb0803617ed7a1cfd14db5cda9c70d0b
Josie <zou_josie@163.com>
2016-05-23 14:03:49 +0800

    refactor: add username to get all whitelist

--- src/main/java/com/josie/quake/controller/FilterWhiteListController.java
+++ src/main/java/com/josie/quake/controller/FilterWhiteListController.java
@@ -4,6 +4,8 @@ import com.josie.quake.commons.Constant;
 import com.josie.quake.commons.utils.ErrorInfo;
 import com.josie.quake.commons.utils.RegexUtils;
 import com.josie.quake.commons.utils.ResponseUtils;
+import com.josie.quake.model.FilterRule;
+import com.josie.quake.model.FilterWhiteList;
 import com.josie.quake.model.User;
 import com.josie.quake.service.FilterWhiteListService;
 import com.josie.quake.service.UserService;
@@ -13,6 +15,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Josie on 16/5/21.
@@ -49,7 +56,18 @@ public class FilterWhiteListController {
         if (user.getPrivilege() == User.Privilege.Common.toInt()) {
             return ResponseUtils.returnError(ErrorInfo.NO_PRIVILEGE);
         }
-        return ResponseUtils.returnOK(filterWhiteListService.getall());
+        List<FilterWhiteList> filterWhiteLists = filterWhiteListService.getall();
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (int i = 0; i < filterWhiteLists.size(); i++) {
+            Map<String, Object> map = new HashMap<>();
+            FilterWhiteList filterWhiteList = filterWhiteLists.get(i);
+            map.put("id", filterWhiteList.getId());
+            map.put("username", userService.getById(filterWhiteList.getOperater()).getUsername());
+            map.put("url", filterWhiteList.getUrl());
+            map.put("createTime", filterWhiteList.getCreateTime());
+            result.add(map);
+        }
+        return ResponseUtils.returnOK(result);
     }
 
     @RequestMapping(value = "delete", produces = Constant.WebConstant.JSON_FORMAT)
